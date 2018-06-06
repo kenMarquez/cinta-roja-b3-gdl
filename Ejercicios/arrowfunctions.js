d24fc0c5b9a72cc0e9a852a5e39ceed188620c6d
Nora Villaseñor <norafer1993@hotmail.com>
2018-06-05 20:41:13 -0500

    exercise

---
+++ Ejercicios/arrowfunctions.js
@@ -0,0 +1,15 @@
+// const suma  = (a,b,c,d,e,f,g) => {
+//        return a+b+c+d+e+f+g;
+// };   
+
+// let resultado = suma (5,1,2,3,8,9,0);
+//  console.log(resultado);
+
+
+
+
+
+
+ let num = (1,2,5,8,7) => {
+     return num => num % 2;
+ };
